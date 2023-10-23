9d40943131962ddf976546a910e897e417a44acd
stecrotti <sky_96@live.it>
2023-10-23 14:59:20 +0200

    remove residue references to outedges

--- src/FactorGraphs/factorgraph.jl
+++ src/FactorGraphs/factorgraph.jl
@@ -4,7 +4,7 @@ const Variable = Right
 """
     FactorGraphVertex
 
-A type to represent a vertex in a bipartite graph, to be passed as an argument to [`neighbors`](@ref), [`inedges`](@ref), [`outedges`](@ref), see examples therein.
+A type to represent a vertex in a bipartite graph, to be passed as an argument to [`neighbors`](@ref), see examples therein.
 It is recommended to use the [`variable`](@ref) and [`factor`](@ref) constructors.
 """
 const FactorGraphVertex = BipartiteGraphVertex
@@ -12,14 +12,14 @@ const FactorGraphVertex = BipartiteGraphVertex
 """
     factor(a::Integer)
 
-Wraps index `a` in a container such that other functions like [`neighbors`](@ref), [`inedges`](@ref) etc. know that it indices a factor node.
+Wraps index `a` in a container such that other functions like [`neighbors`](@ref) know that it indices a factor node.
 """
 factor(a::Integer) = vertex(a, Factor)
 
 """
     variable(i::Integer)
 
-Wraps index `i` in a container such that other functions like [`neighbors`](@ref), [`inedges`](@ref) etc. know that it indices a variable node.
+Wraps index `i` in a container such that other functions like [`neighbors`](@ref) know that it indices a variable node.
 """
 variable(i::Integer) = vertex(i, Variable)
 
@@ -134,32 +134,27 @@ end
 
 Return a lazy iterators to the indices of the edges incident on vertex `v`, with `v`.
 
-You should use it as a lightweight alternative to [`inedges`](@ref), [`outedges`](@ref) when the indices of the edges are all you care about.
-
 The output of `edge_indices` does not allocate and it can be used to index external arrays of properties directly
 
 Examples
 ========
 
 ```jldoctest edge_indices
-julia> using BeliefPropagation.FactorGraphs, Test
+julia> using BeliefPropagation.FactorGraphs, Random
 
 julia> g = FactorGraph([0 1 1 0;
                         1 0 0 0;
                         0 0 1 1])
 FactorGraph{Int64} with 3 factors, 4 variables and 5 edges
 
-julia> edgeprops = randn(ne(g));
+julia> edgeprops = randn(MersenneTwister(0), ne(g));
 
-julia> indices = (idx(e) for e in outedges(g, variable(3)));
+julia> indices = edge_indices(g, variable(3));
 
-julia> indices_noalloc = edge_indices(g, variable(3));
-
-julia> @assert edgeprops[collect(indices)] == edgeprops[indices_noalloc]
-
-julia> @test_throws ArgumentError edgeprops[indices]
-Test Passed
-      Thrown: ArgumentError
+julia> edgeprops[indices]
+2-element Vector{Float64}:
+ -0.3530074003005963
+ -0.13485387193052173
 ```
 """
 function edge_indices(g::FactorGraph, a::FactorGraphVertex{Factor})
