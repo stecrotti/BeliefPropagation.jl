2bd9a19a9d7abfab3bdc968e225b9c22b3e41678
Alfredo Braunstein <alfredo.braunstein@polito.it>
2023-11-06 12:11:47 +0100

    type parametrization of MS

--- src/maxsum.jl
+++ src/maxsum.jl
@@ -29,28 +29,28 @@ function update_v_ms!(bp::BP, i::Integer, hnew, bnew, damp::Real, rein::Real,
     return errv, errb
 end
 
-struct MS <: Real
-    val::Float64
-    MS(v::Float64) = new(v)
+struct MS{T} <: Real
+    val::T
+    MS(v::T) where {T<:Union{Float64, Int}} = new{T}(v)
 end
 
-Base.:(+)(x::MS, y::MS) = MS(max(x.val, y.val))
-Base.:(+)(x::MS, y::Float64) = MS(max(x.val, log(y)))
-Base.:(+)(y::Float64, x::MS) = x + y
-Base.:(*)(x::MS, y::MS) = MS(x.val + y.val)
-Base.:(*)(x::MS, y::Float64) = MS(x.val + log(y))
-Base.:(*)(y::Float64, x::MS) = x * y
-Base.one(::Type{MS}) = MS(0.0)
-Base.zero(::Type{MS}) = MS(-Inf)
+Base.:(+)(x::MS, y::Union{Float64, Int, MS}) = MS(max(x.val, log(y)))
+Base.:(+)(y::Union{Float64, Int}, x::MS) = x + y
+Base.:(*)(x::MS, y::Union{Float64, Int, MS}) = MS(x.val + log(y))
+Base.:(*)(y::Union{Float64, Int}, x::MS) = x * y
+Base.log(y::MS) = y.val
+Base.one(::Type{MS{T}}) where {T<:Union{Float64, Int}} = MS(zero(T))
+Base.zero(::Type{MS{T}}) where {T<:Union{Float64, Int}} = MS(typemin(T))
 
 
 function update_f_ms!(bp::BP, a::Integer, unew, damp::Real,
         f::BetheFreeEnergy; extra_kwargs...)
     (; g, ψ, u, h) = bp
     ea = edge_indices(g, factor(a))
-    hflat = mortar([mappedarray(MS, h[ai]) for ai in ea])
+    hflat = mortar([mappedarray(x->MS(x), h[ai]) for ai in ea])
     uflat = mortar([mappedarray(x->MS(x), x->x.val, unew[ai]) for ai in ea])
-    ret = DiffResults.DiffResult(MS(0.0), uflat)
+    #@show typeof(hflat) typeof(uflat) typeof(h[ea])
+    ret = DiffResults.DiffResult(MS(zero(eltype(uflat))), uflat)
     ForwardDiff.gradient!(ret, hin -> compute_za(ψ[a], hin.blocks), hflat)
     logzₐ = DiffResults.value(ret).val
     f.factors[a] = -logzₐ
