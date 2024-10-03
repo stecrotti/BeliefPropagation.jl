d85ace4fa5e092e47ed7d3e808b4c086ad319730
stecrotti <sky_96@live.it>
2024-10-03 18:04:04 +0200

    docs for new factors API

--- src/Models/ksat.jl
+++ src/Models/ksat.jl
@@ -3,8 +3,6 @@ struct KSATClause{T}  <: BPFactor where {T<:AbstractVector{<:Bool}}
     J :: T 
 end
 
-Base.eltype(::KSATClause) = eltype(1.0)
-
 function (f::KSATClause)(x) 
     isempty(x) && return 1.0
     return any(xᵢ - 1 != Jₐᵢ for (xᵢ, Jₐᵢ) in zip(x, f.J)) |> float
