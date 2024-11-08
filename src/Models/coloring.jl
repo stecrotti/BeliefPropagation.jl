445453c6db98c2a5ab17e1bfbcaa0aeccda33067
stecrotti <sky_96@live.it>
2024-11-08 02:23:22 +0100

    add docs and notebook

--- src/Models/coloring.jl
+++ src/Models/coloring.jl
@@ -1,3 +1,10 @@
+@doc raw"""
+    ColoringCoupling
+
+A type of [`BPFactor`](@ref) representing a factor in the coloring problem.
+It always involves two (discrete) incident variables $x_i$, $x_j$. The factor evaluates to
+``\psi(x_i,x_j)=1-\delta(x_i,x_j)``
+"""
 struct ColoringCoupling  <: BPFactor; end
 
 function (f::ColoringCoupling)(x)
@@ -15,8 +22,25 @@ end
 
 Base.eltype(f::ColoringCoupling) = typeof(f((1,1)))
 
+@doc raw"""
+    SoftColoringCoupling
+
+A soft version of [`ColoringCoupling`](@ref).
+It always involves two (discrete) incident variables $x_i$, $x_j$. The factor evaluates to
+``\psi(x_i,x_j)=e^{-\beta\delta(x_i,x_j)}``
+
+Fields
+========
+
+- `β`: the real parameter controlling the softness. A `ColoringCoupling` is recovered in the large β limit.
+"""
 struct SoftColoringCoupling{T<:Real} <: BPFactor
     β :: T
+
+    function SoftColoringCoupling(β::T) where {T<:Real}
+        β > 0 || throw(ArgumentError("Parameter β must be positive, got $β"))
+        return new{T}(β)
+    end
 end
 
 function (f::SoftColoringCoupling)(x)
